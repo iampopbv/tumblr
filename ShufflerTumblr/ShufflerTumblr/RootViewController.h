2ff58f588545b2d79e673e034ad41e56a2c14bf0
CasperCL <casperjeekhof@gmail.com>
2013-05-05 14:13:37 +0200

    added support for pageviewcontroller for the posts

---
+++ ShufflerTumblr/ShufflerTumblr/RootViewController.h
@@ -0,0 +1,15 @@
+//
+//  RootViewController.h
+//  PageBasedAppTest
+//
+//  Created by Casper Eekhof on 05-05-13.
+//  Copyright (c) 2013 Casper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
