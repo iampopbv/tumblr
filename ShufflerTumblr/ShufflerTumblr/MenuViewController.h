41b2e76240a3f376432d6a14093d2deaee20e09f
oudj001 <Justin.oud@hotmail.com>
2013-06-10 14:51:51 +0200

    changed layout for casper

--- ShufflerTumblr/ShufflerTumblr/MenuViewController.h
+++ ShufflerTumblr/ShufflerTumblr/MenuViewController.h
@@ -12,7 +12,7 @@
 #import "Blog.h"
 #import "bloggetter.h"
 
-@interface MenuViewController : UIViewController
+@interface MenuViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 
 @property Reachability *internetReachableChecker;
 @property BOOL hasInternet;
@@ -21,15 +21,29 @@
 @property Blog *blog2;
 @property Blog *blog3;
 @property Blog *blog4;
+@property Blog *blog5;
+@property Blog *segueBlog;
+@property UIStoryboardSegue *mainSegue;
 
 
 @property (weak, nonatomic) IBOutlet UILabel *blog1Title;
 @property (weak, nonatomic) IBOutlet UILabel *blog2Title;
 @property (weak, nonatomic) IBOutlet UILabel *blog3Title;
 @property (weak, nonatomic) IBOutlet UILabel *blog4Title;
+@property (weak, nonatomic) IBOutlet UILabel *blog5Title;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageBlog1;
 @property (weak, nonatomic) IBOutlet UIImageView *imageBlog2;
 @property (weak, nonatomic) IBOutlet UIImageView *imageBlog3;
 @property (weak, nonatomic) IBOutlet UIImageView *imageBlog4;
+@property (weak, nonatomic) IBOutlet UIImageView *imageBlog5;
+@property (weak, nonatomic) IBOutlet UINavigationItem *Shumblr;
+@property (weak, nonatomic) IBOutlet UIButton *signupbutton;
+@property (weak, nonatomic) IBOutlet UILabel *listento;
+
+@property NSMutableArray *tabledata;
+@property NSMutableArray *tableimages;
+@property NSMutableArray *blogdata;
 @end
