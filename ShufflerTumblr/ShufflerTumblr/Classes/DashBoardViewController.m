969612f113b5c9f1534fe0e57d2dd9b86e2c3c4f
Adrian Zborowski <ak.zborowski@gmail.com>
2014-04-14 12:30:56 +0200

    + Implemented design made by Thanus

--- ShufflerTumblr/ShufflerTumblr/Classes/DashBoardViewController.m
+++ ShufflerTumblr/ShufflerTumblr/Classes/DashBoardViewController.m
@@ -157,7 +157,7 @@
 
 #pragma UITableView delegates
 -(void) tableView:(UITableView *) tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    chosenRow = indexPath.row;
+    chosenRow = (int)indexPath.row;
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
     
