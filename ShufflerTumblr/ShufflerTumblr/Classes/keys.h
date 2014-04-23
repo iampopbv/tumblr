b4f4058ef59db5359cfc49bbae9f3fd1ffbb59d4
Adrian Zborowski <ak.zborowski@gmail.com>
2014-04-23 11:27:30 +0200

    + Keys comments

--- ShufflerTumblr/ShufflerTumblr/Classes/keys.h
+++ ShufflerTumblr/ShufflerTumblr/Classes/keys.h
@@ -9,9 +9,11 @@
 #ifndef ShufflerTumblr_keys_h
 #define ShufflerTumblr_keys_h
 
+// Last years students keys
 //#define kConsumerKey		@"9DTflrfaaL6XIwUkh1KidnXFUX0EQUZFVEtjwcTyOLNsUPoWLV"
 //#define kConsumerSecret		@"08SjC79ZiQPqY8Dn8kg0Rn2OxIPrNb8xEuNbB7Op7rMW0VFdOs"
 
+// Keys registered by Adrian Zborowski
 #define kConsumerKey		@"NPIxO1R794eabwFPmWLuqhsyMxYcYIXwGBCALvOlzNFoaCt378"
 #define kConsumerSecret		@"pVOq6DkuOjNjUy52oXXX2iKzMJl9gcfIPVkutFjkmzrxjPfqMc"
 
